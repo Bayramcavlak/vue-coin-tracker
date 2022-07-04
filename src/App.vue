44260609034893b1872c0485a3a220273e6b4176
Bayram Cavlak <bcavlak@gmail.com>
2022-07-04 17:32:47 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App Bu bir örnek"/>
+  <CounterAds title="Bu bir sayaç başlığıdır." num="10" />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import CounterAds from './components/CounterAds.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    CounterAds
   }
 }
 </script>
---
+++ src/components/CounterAds.vue
@@ -0,0 +1,32 @@
+<template>
+<h2>{{title}} {{num}}</h2>
+  <div>
+    {{ counter }} <button @click="counter++" class="counter">Counter +</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CounterAds",
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  props: {
+    title: String,
+    num: Number
+  }
+};
+</script>
+
+<style scoped>
+.counter {
+  background-color: #fef595;
+  border-radius: 15px;
+  padding: 10px;
+  margin: 10px;
+  border: #fef595 1px solid;
+  cursor: pointer;
+}
+</style>
